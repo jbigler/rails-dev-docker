31bff8085e6196cb9af2ab1f6db039b41b463ab8
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2026-03-09 19:00:22 +0700

    Fix Nvim docker settings

--- docker-config/entrypoint-nvim.sh
+++ docker-config/entrypoint-nvim.sh
@@ -4,10 +4,13 @@ set -xeuo pipefail
 mkdir -p ~/.config
 mkdir -p ~/.local
 
+export GIT_TERMINAL_PROMPT=0
+
 if [[ ! -f ~/.config/nvim/init.lua ]]; then
 	git clone --depth 1 https://github.com/jbigler/AstroNvim ~/.config/nvim
 else
-	git -C ~/.config/nvim pull
+	git -C ~/.config/nvim remote set-url origin https://github.com/jbigler/AstroNvim.git
+	git -C ~/.config/nvim pull --ff-only
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
