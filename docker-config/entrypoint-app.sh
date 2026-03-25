23df6282a7954cd876d386b83c0ece5ac702f039
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2026-03-25 16:13:13 +0700

    Dump and restore rustfs as well

--- docker-config/entrypoint-app.sh
+++ docker-config/entrypoint-app.sh
@@ -4,6 +4,7 @@ set -e
 if bin/rails runner "ActiveRecord::Base.connection.table_exists?('schema_migrations')" &>/dev/null; then
   echo "Database already initialized — running migrations..."
   bin/rails db:migrate
+  bin/rails db:test:prepare
 else
   echo "Preparing database from scratch..."
   bin/rails db:prepare
