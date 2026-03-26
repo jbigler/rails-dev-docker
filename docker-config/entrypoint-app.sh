1e43828f984af7e2c63b6cd5af25f141339a973c
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2026-03-26 16:28:48 +0700

    Run bundle install on app start

--- docker-config/entrypoint-app.sh
+++ docker-config/entrypoint-app.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+bundle install
+
 if bin/rails runner "ActiveRecord::Base.connection.table_exists?('schema_migrations')" &>/dev/null; then
   echo "Database already initialized — running migrations..."
   bin/rails db:migrate
