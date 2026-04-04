05d261ce83c077976fc52029db2a1e6359148578
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2026-04-04 09:08:55 +0700

    Support gem volume by ruby version

--- docker-config/entrypoint-app.sh
+++ docker-config/entrypoint-app.sh
@@ -2,6 +2,7 @@
 set -e
 
 bundle install
+npm install
 
 if bin/rails runner "ActiveRecord::Base.connection.table_exists?('schema_migrations')" &>/dev/null; then
   echo "Database already initialized — running migrations..."
